21f581209740f4f8ecbcc6437dedc5da9bd57971
W1zzA <W1zzA@W1ZZK1>
2022-09-30 23:11:26 +0300

    Добавьте файлы проекта.

---
+++ Lab_7/Lab_7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+/*
+Лабораторная работа №7. Оператор цикла с предусловием
+12. Дано целое число N > 1. 
+Вывести наибольшее из целых чисел K, для которых сумма 1 + 2 + ... + K будет меньше или равна N, и саму эту сумму.
+*/
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int N;
+	int k = 0;
+	int sum = 0;
+	cin >> N;
+
+	while (sum <= N) {
+		k += 1;
+		sum += k;
+	}
+	cout << k << endl;
+	cout << sum;
+	return 0;
+}
